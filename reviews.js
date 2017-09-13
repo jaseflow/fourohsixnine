52a1865712b3c74739bba88a208e59745820918d
Jason Corbett <jcorbett@expedia.com>
2017-09-13 14:32:56 +1000

    Init commit

---
+++ reviews.js
@@ -0,0 +1,198 @@
+var reviews = [
+    {
+        rating: 4,
+        author: "Richard Phillips",
+        date: "31 March 2017",
+        title: "Extremely kind and helpful",
+        review: "Clinton made me feel comfortable throughout the entire process. I highly recommend."
+    },
+    {
+        rating: 5,
+        author: "Paul Thompson",
+        date: "18 March 2017",
+        title: "Second to none",
+        review: "From now on whenever myself or a friend is looking to sell their property, I will recommend Clinton in a heartbeat."
+    },
+    {
+        rating: 4,
+        author: "Robyn Funston",
+        date: "12 March 2017",
+        title: "Look no further",
+        review: "Clinton is the man for the job!"
+    },
+    {
+        rating: 5,
+        author: "Tom Harrison",
+        date: "3 March 2017",
+        title: "Really enjoyed working with Clinton",
+        review: "Nothing but good things to say about Clinton. Super profession from the moment I met him. Great job."
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "28 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 4,
+        author: "Jill Jackson",
+        date: "16 February 2017",
+        title: "Amazing job",
+        review: "Met all my needs as a seller. I highly recommend Clinton to anyone thinking about selling."
+    },
+    {
+        rating: 5,
+        author: "Paul Francis",
+        date: "14 March 2017",
+        title: "Yes yes yes",
+        review: "Clinton went above and beyond for my wife and I. So so happy."
+    },
+    {
+        rating: 5,
+        author: "Robyn Funston",
+        date: "12 March 2017",
+        title: "Look no further",
+        review: "Clinton is the man for the job!"
+    },
+    {
+        rating: 4,
+        author: "Tom Harrison",
+        date: "3 March 2017",
+        title: "Really enjoyed working with Clinton",
+        review: "Nothing but good things to say about Clinton. Super profession from the moment I met him. Great job."
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    },
+    {
+        rating: 5,
+        author: "Jack Stephenson",
+        date: "7 February 2017",
+        title: "Clinton is the man!",
+        review: "Clinton got us top dollar. I am so glad we crossed paths!"
+    }
+]
